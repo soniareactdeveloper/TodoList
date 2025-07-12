5d143722d4a42e22fe9172230d0956024d832188
Sonia Akter <168516331+soniareactdeveloper@users.noreply.github.com>
2025-07-13 00:13:21 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,79 +1,65 @@
-let error          = document.querySelector(".error")
-let todoInput      = document.querySelector('.todoInput')
-let todoButton     = document.querySelector('.todoButton')
-let todoList       = document.querySelector('.todoList')
-let box            = document.querySelector('.box')
-let result         = document.querySelector ('.result')
-let i              = 0
+let result = document.querySelector(".result");
+let error = document.querySelector(".error");
+let todoInput = document.querySelector(".todoInput");
+let todoButton = document.querySelector(".todoButton");
+let todoList = document.querySelector(".todoList");
+let i = 0;
 
-todoButton.addEventListener('click' , ()=>{
-  if(todoInput.value == '') {
-    error.innerHTML = "Please Enter Your ToDo"
-  }else {
-     
-     error.innerHTML = ''
-     i++;
-     result.innerHTML    = i
+todoButton.addEventListener("click", () => {
+  const inputVal = todoInput.value.trim();
 
-
-
-    // creating tags 
-    let singleTodo      = document.createElement('div')
-    let todoSingleInput = document.createElement('input')
-    let editBtn         = document.createElement('button')
-    let edit            = document.createElement('span')
-    let deleteBtn       = document.createElement('button')
-   
-
-    
-    // append child
-    todoList.appendChild(singleTodo)
-    singleTodo.appendChild(todoSingleInput)
-    singleTodo.appendChild(editBtn)
-    editBtn.appendChild(edit)
-    singleTodo.appendChild(deleteBtn)
-  
-    // class list
-    singleTodo.classList.add("singleTodo")
-    todoSingleInput.classList.add("todoSingleInput")
-    editBtn.classList.add('editBtn')
-    edit.classList.add('edit','fa-solid' , 'fa-user-pen')
-    deleteBtn.classList.add('deleteBtn', 'fa-solid', 'fa-trash-can')
-    
-
-    // add content
-    todoSingleInput.value = todoInput.value
-    todoInput.value       = '' 
-    editBtn.innerHTML     = 'Edit'
-    editBtn.style         = "font-size:24px;"
-    
-
-
-   //set attribute
-   todoSingleInput.setAttribute('readonly','readonly')
-
-   
-  //  delete button 
-   deleteBtn.addEventListener('click' , ()=>{
-    singleTodo.remove()
-    i--;
-    result.innerHTML    = i
-
-   })
-   
-  // edit button
-  editBtn.addEventListener('click' , ()=>{
-    if(editBtn.innerHTML == "Edit"){
-      editBtn.innerHTML = "Save"
-      editBtn.style     = "color: yellow; font-size:22px;"
-      todoSingleInput.removeAttribute('readonly', 'readonly')
-
-    }else {
-      editBtn.innerHTML  = "Edit"
-      editBtn.style     = "color:#000; font-size:22px;"
-      todoSingleInput.setAttribute('readonly','readonly')
-    }
-  })
-
+  if (inputVal === "") {
+    error.textContent = "Please enter a value";
+    return;
   }
-})
+
+  error.textContent = "";
+  i++;
+  result.textContent = i;
+
+  // Create todo container
+  const singleTodo = document.createElement("div");
+  singleTodo.classList.add("singleTodo");
+
+  // Create input
+  const todoSingleInput = document.createElement("input");
+  todoSingleInput.classList.add("todoSingleInput");
+  todoSingleInput.value = inputVal;
+  todoSingleInput.setAttribute("readonly", true);
+
+  // Create edit button
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("editBtn");
+  editBtn.innerHTML = `<i class="fa-solid fa-user-pen"></i>`;
+
+  // Create delete button
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("deleteBtn");
+  deleteBtn.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+
+  // Append to todo div
+  singleTodo.appendChild(todoSingleInput);
+  singleTodo.appendChild(editBtn);
+  singleTodo.appendChild(deleteBtn);
+  todoList.appendChild(singleTodo);
+
+  // Clear input
+  todoInput.value = "";
+
+  // Edit functionality
+  editBtn.addEventListener("click", () => {
+    if (todoSingleInput.hasAttribute("readonly")) {
+      todoSingleInput.removeAttribute("readonly");
+      todoSingleInput.focus();
+    } else {
+      todoSingleInput.setAttribute("readonly", true);
+    }
+  });
+
+  // Delete functionality
+  deleteBtn.addEventListener("click", () => {
+    todoList.removeChild(singleTodo);
+    i--;
+    result.textContent = i;
+  });
+});
